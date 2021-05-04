4eb3a4253b3ae395451b43b39c859d811a6c3c84
gmcgoldr <garrin.mcgoldrick@gmail.com>
2021-05-04 09:58:36 -0400

    add clear to faust processor interface

--- faust2hpp/Source/FaustProcessor.h
+++ faust2hpp/Source/FaustProcessor.h
@@ -7,6 +7,7 @@ public:
   FaustProcessor() = default;
   virtual ~FaustProcessor() = default;
   virtual void reset() = 0;
+  virtual void clear() = 0;
   virtual void prepare(int sampleRate) = 0;
   virtual void process(int count, FAUSTFLOAT** buffer) = 0;
 };
